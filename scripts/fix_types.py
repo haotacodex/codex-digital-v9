ff0278207363d38ef37856e250f165e80d6fa4d6
Hao Ta Minh <haomt92@gmail.com>
2026-05-13 14:20:07 +0700

    Fix icon type for Vercel build

---
+++ scripts/fix_types.py
@@ -0,0 +1,10 @@
+﻿import re, sys
+filepath = 'components/sections/ServicesStrip.tsx'
+with open(filepath, encoding='utf-8') as f:
+    c = f.read()
+c = c.replace(chr(39) + 'aria-hidden' + chr(39) + '?: string | boolean', chr(39) + 'aria-hidden' + chr(39) + '?: Booleanish | undefined')
+if 'Booleanish' not in c:
+    c = c.replace('from ' + chr(39) + '@/lib/types' + chr(39), 'from ' + chr(39) + '@/lib/types' + chr(39) + chr(10) + 'import type { Booleanish } from ' + chr(39) + 'react' + chr(39))
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(c)
+print('Fixed')
